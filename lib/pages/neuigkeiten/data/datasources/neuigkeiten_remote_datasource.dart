abb157384814ce664d59682d6218d2db512bae30
NachtaktiverHalbaffe <NachtaktiverHalbaffe@users.noreply.github.com>
2020-03-26 23:56:02 +0100

    Neuigkeiten:Erste Implementierung Onlineanbindung

--- lib/pages/neuigkeiten/data/datasources/neuigkeiten_remote_datasource.dart
+++ lib/pages/neuigkeiten/data/datasources/neuigkeiten_remote_datasource.dart
@@ -1,10 +1,24 @@
+import 'dart:convert';
+
+import 'package:eje/core/error/exception.dart';
 import 'package:eje/pages/neuigkeiten/data/models/neuigkeit_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 class NeuigkeitenRemoteDatasource {
+  final http.Client client;
+  final String apiUrl;
+
+  NeuigkeitenRemoteDatasource({@required this.client, @required this.apiUrl});
+
   //TODO: Implementierung der Onlineanbindung
 
-  Future<List<NeuigkeitenModel>> getNeuigkeiten() {
-
+  Future<List<NeuigkeitenModel>> getNeuigkeiten() async {
+    final response = await client.get(apiUrl);
+    if (response.statusCode == 200) {
+      // return NeuigkeitenModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
   }
 }
