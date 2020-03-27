d452a4cba12aa207b9291501c962b699155f0b73
NachtaktiverHalbaffe <NachtaktiverHalbaffe@users.noreply.github.com>
2020-03-27 16:50:00 +0100

    Neuigkeiten: Bloc Events und States implementiert

--- lib/pages/neuigkeiten/presentation/bloc/neuigkeiten_bloc_event.dart
+++ lib/pages/neuigkeiten/presentation/bloc/neuigkeiten_bloc_event.dart
@@ -1,5 +1,19 @@
+import 'package:eje/pages/neuigkeiten/domain/usecases/GetNeuigkeit.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
+@immutable
 abstract class NeuigkeitenBlocEvent extends Equatable {
-  const NeuigkeitenBlocEvent();
+  NeuigkeitenBlocEvent([List props = const <dynamic>[]]) : super(props);
 }
+
+// Zeige ganzen Artikel wenn Button geklicket wird
+class GetNeuigkeitDetails extends NeuigkeitenBlocEvent {
+  final String titel;
+
+  //Constructor
+  GetNeuigkeitDetails(this.titel) : super([titel]);
+}
+
+// Refreshe Neuigkeiten, wenn Liste zum refresen runter gezogen wird
+class RefreshNeuigkeiten extends NeuigkeitenBlocEvent {}
--- lib/pages/neuigkeiten/presentation/bloc/neuigkeiten_bloc_state.dart
+++ lib/pages/neuigkeiten/presentation/bloc/neuigkeiten_bloc_state.dart
@@ -1,10 +1,29 @@
+import 'package:eje/pages/neuigkeiten/domain/entitys/neuigkeit.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
+@immutable
 abstract class NeuigkeitenBlocState extends Equatable {
-  const NeuigkeitenBlocState();
+  NeuigkeitenBlocState([List props = const <dynamic>[]]) : super(props);
 }
 
-class InitialNeuigkeitenBlocState extends NeuigkeitenBlocState {
-  @override
-  List<Object> get props => [];
+//State wenn keine Neuigkeit vorhanden ist
+class Empty extends NeuigkeitenBlocState {}
+
+//State wenn Neuigkeiten geladen werden
+class Loading extends NeuigkeitenBlocState {}
+
+// State wenn Neuigkeien fertig geladen sind
+class Loaded extends NeuigkeitenBlocState {
+  final Neuigkeit neuigkeit;
+
+  //Constructor
+  Loaded({@required this.neuigkeit}):super([neuigkeit]);
+}
+
+class Error extends NeuigkeitenBlocState {
+  final String message;
+
+  //Constructor
+  Error({@required this.message}):super([message]);
 }
