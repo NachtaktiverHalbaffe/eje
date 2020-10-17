ba1f41b20f53a6eae018a9f07ef0aeb78704a0ef
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2020-10-17 15:39:38 +0200

    Fixed MaterialNavigator

--- lib/core/widgets/HyperlinkSection.dart
+++ lib/core/widgets/HyperlinkSection.dart
@@ -1,7 +1,5 @@
 import 'package:eje/core/utils/injection_container.dart';
-import 'package:eje/core/widgets/DetailsPage.dart';
 import 'package:eje/pages/articles/articlesPage.dart';
-import 'package:eje/pages/articles/domain/entity/Article.dart';
 import 'package:eje/pages/articles/domain/entity/Hyperlink.dart';
 import 'package:eje/pages/articles/presentation/bloc/articles_bloc.dart';
 import 'package:flutter/material.dart';
@@ -51,18 +49,18 @@ Widget _column(Hyperlink hyperlink, BuildContext context, bool isCacheEnabled) {
           throw 'Could not launch $hyperlink.link';
         }
       } else {
-        () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => BlocProvider.value(
-                  value: sl<ArticlesBloc>(),
-                  child: ArticlesPage(
-                    isCacheEnabled: isCacheEnabled,
-                    url: hyperlink.link,
-                  ),
-                ),
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => BlocProvider.value(
+              value: sl<ArticlesBloc>(),
+              child: ArticlesPage(
+                isCacheEnabled: isCacheEnabled,
+                url: hyperlink.link,
               ),
-            );
+            ),
+          ),
+        );
       }
     },
     child: Column(
@@ -78,6 +76,7 @@ Widget _column(Hyperlink hyperlink, BuildContext context, bool isCacheEnabled) {
             ),
             Text(
               hyperlink.description,
+              overflow: TextOverflow.ellipsis,
               style: TextStyle(
                 fontSize: 48 / MediaQuery.of(context).devicePixelRatio,
                 decoration: TextDecoration.underline,
