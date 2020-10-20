ccc2ad22f31720c5b2db5fc3c4605e5b5efec854
NachtaktiverHalbaffe <{e-A17XEv%q7U&>
2020-10-20 18:25:03 +0200

    Migration to android embedding v2

--- android/app/src/main/kotlin/de/eje_esslingen/eje/MainActivity.kt
+++ android/app/src/main/kotlin/de/eje_esslingen/eje/MainActivity.kt
@@ -1,13 +1,8 @@
 package de.eje_esslingen.eje
 
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+
 }
