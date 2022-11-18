bfbde16029b195e7e0e5ad403607d8376b5ff5ec
Felix Brugger <felixbrugger@tutanota.com>
2022-11-18 22:49:26 +0100

    Added splash screen generation and changed rss feed deps

--- lib/pages/neuigkeiten/data/datasources/news_remote_datasource.dart
+++ lib/pages/neuigkeiten/data/datasources/news_remote_datasource.dart
@@ -2,8 +2,8 @@ import 'package:eje/app_config.dart';
 import 'package:eje/core/error/exception.dart';
 import 'package:eje/pages/neuigkeiten/domain/entitys/news.dart';
 import 'package:http/http.dart' as http;
-import 'package:dart_rss/dart_rss.dart';
 import 'package:http/http.dart';
+import 'package:webfeed/domain/rss_feed.dart';
 
 class NewsRemoteDatasource {
   final client = http.Client();
@@ -22,7 +22,7 @@ class NewsRemoteDatasource {
       throw ConnectionException();
     }
     if (response.statusCode == 200) {
-      var payload = RssFeed.parse(response.body).items.toList();
+      var payload = RssFeed.parse(response.body).items!.toList();
 
       // Parsing
       if (payload.isNotEmpty) {
@@ -42,7 +42,8 @@ class NewsRemoteDatasource {
                   ? [""]
                   : [payload[i].enclosure!.url ?? ""],
               link: payload[i].link ?? "",
-              published: parseDateTimeFromRSS(payload[i].pubDate ?? ""),
+              published:
+                  parseDateTimeFromRSS(payload[i].pubDate.toString() ?? ""),
             ),
           );
         }
@@ -56,40 +57,14 @@ class NewsRemoteDatasource {
   }
 
   DateTime parseDateTimeFromRSS(String dateFromSource) {
-    //? Format from RSS-Feed: Mon, 15 Mar 21 09:00:00 +0100
+    //? Format from RSS-Feed: 0022-08-17 14:59:00.000
     //Parse elements from unformatted DateTime
-    int day = int.parse(dateFromSource.substring(5, 7));
-    String monthString = dateFromSource.substring(8, 11);
-    int month = 1;
-    if (monthString.toLowerCase() == "jan") {
-      month = 1;
-    } else if (monthString.toLowerCase() == "feb") {
-      month = 2;
-    } else if (monthString.toLowerCase() == "mar") {
-      month = 3;
-    } else if (monthString.toLowerCase() == "apr") {
-      month = 4;
-    } else if (monthString.toLowerCase() == "may") {
-      month = 5;
-    } else if (monthString.toLowerCase() == "jun") {
-      month = 6;
-    } else if (monthString.toLowerCase() == "jul") {
-      month = 7;
-    } else if (monthString.toLowerCase() == "aug") {
-      month = 8;
-    } else if (monthString.toLowerCase() == "sep") {
-      month = 9;
-    } else if (monthString.toLowerCase() == "oct") {
-      month = 10;
-    } else if (monthString.toLowerCase() == "nov") {
-      month = 11;
-    } else if (monthString.toLowerCase() == "dec") {
-      month = 12;
-    }
-    int year = int.parse("20${dateFromSource.substring(12, 14)}");
-    int hour = int.parse(dateFromSource.substring(15, 17));
-    int minutes = int.parse(dateFromSource.substring(18, 20));
-    int seconds = int.parse(dateFromSource.substring(21, 23));
+    int day = int.parse(dateFromSource.substring(8, 10));
+    int month = int.parse(dateFromSource.substring(5, 7));
+    int year = int.parse("20${dateFromSource.substring(2, 4)}");
+    int hour = int.parse(dateFromSource.substring(11, 13));
+    int minutes = int.parse(dateFromSource.substring(14, 16));
+    int seconds = int.parse(dateFromSource.substring(17, 19));
     return DateTime(year, month, day, hour, minutes, seconds);
   }
 }
