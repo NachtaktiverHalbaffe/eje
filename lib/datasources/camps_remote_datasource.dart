b522fc90ed3130a5e99ebd600a84b701b7283a15
NachtaktiverHalbaffe <LeMonkay.VT&IT@tuanota.com>
2024-01-21 23:16:49 +0100

    Bugfix

--- lib/datasources/camps_remote_datasource.dart
+++ lib/datasources/camps_remote_datasource.dart
@@ -146,7 +146,7 @@ class CampsRemoteDatasource implements RemoteDataSource<Camp, int> {
   int _parsePrice(responseData) {
     // TODO fix price parsing
     if (responseData != null) {
-      if (int.tryParse(responseData.replaceAll(new RegExp(r"\D"), "") != null)) {
+      if (int.tryParse(responseData.replaceAll(new RegExp(r"\D"), "")) != null) {
         return int.tryParse(responseData.replaceAll(new RegExp(r"\D"), "")) ?? 0;
       } else {
         return 0;
